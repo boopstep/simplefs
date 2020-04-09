6fda952a8be13fb9da86074b6c9ffdf1af7f3a1d
jafow <jared.a.fowler@gmail.com>
2020-04-08 22:50:03 -0700

    rename to BITMAP_LIMIT

--- fileblock/src/simplefs.rs
+++ fileblock/src/simplefs.rs
@@ -1,13 +1,13 @@
 // slightly arbitrary size of our "disk": 4GIB, broken into 4KiB blocks
 // 4 * 2.pow(30) / 4096 == 2Kib required to store bitmap
-const BITMAP_LEN: usize = 1048576_usize;
+const BITMAP_LIMIT: usize = 1048576_usize;
 
 /// disk block free list 
 /// persistent tracking of what blocks have been allocated.
 /// option 1: Bitmap! naive style
 struct  DiskBlockFreelist {
     // keep an array of bits indicating whether a file
-    bits: [usize; BITMAP_LEN]
+    bits: [usize; BITMAP_LIMIT]
 }
 
 /// option 2: B-Tree!!
@@ -19,7 +19,7 @@ struct DiskBlockFreeTree {
 struct Node {
     val: u32,
     // don't know how to imlement this yet without a Box; is it even possible?
-    // children: [DiskBlockFreeTree; BITMAP_LEN]
+    // children: [DiskBlockFreeTree; BITMAP_LIMIT]
 }
 
 
