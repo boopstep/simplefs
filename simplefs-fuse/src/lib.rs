a9175839c46646ef4b5efafec0a11a1beec2bbc6
jafow <jared.a.fowler@gmail.com>
2020-05-02 16:45:00 -0700

    wip: expose fuse_main

--- simplefs-fuse/src/lib.rs
+++ simplefs-fuse/src/lib.rs
@@ -9,6 +9,14 @@ pub struct fuse_args {
     pub allocated: c_int
 }
 
+#[repr(C)]
+#[derive(Debug)]
+pub struct fuse_operations {
+    //  there are like 2 dozen more to add...
+	pub readlink: *const *const c_char
+}
+
 extern "C" {
     pub fn fuse_mount_compat25(mountpoint: *const c_char, args: *const fuse_args) -> c_int;
+    pub fn fuse_main(args: *const fuse_args, op: *const fuse_operations, private_data: *const c_char) -> c_int;
 }
