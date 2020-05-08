e70ffeb073f7b34fe2499969be2417ea916f5f14
jafow <jared.a.fowler@gmail.com>
2020-05-07 22:03:50 -0700

    remove link to fuse3 - we are ok with 2.9

--- simplefs-fuse/build.rs
+++ simplefs-fuse/build.rs
@@ -4,8 +4,5 @@ pub fn main() {
    // link to the fuse lib 
    println!("cargo:rustc-link-lib=dylib=fuse");
 
-   // link to fuse3
-   println!("cargo:rustc-link-lib=dylib=fuse3");
-
    pkg_config::Config::new().atleast_version("2.9.6").probe("fuse").unwrap();
 }
